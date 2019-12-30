73403e934747ea1d06638ae123c555fa5d1789dd
sanderjson <sanderson.jma@gmail.com>
2019-12-29 23:16:04 -0700

    initial commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,17 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './site/**/*.html',
+  ],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
 module.exports = {
-  plugins: {
-    "postcss-import": {},
-    "postcss-preset-env": {
+  plugins: [
+    require("postcss-import"),
+    require("postcss-preset-env")({
       browsers: "last 2 versions"
-    },
-    autoprefixer: {}
-  }
+    }),
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production' ? [purgecss] : []
+  ]
 };
